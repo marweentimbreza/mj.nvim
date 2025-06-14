1169c89de6545f7757a0400dbafc1f06ea526d51
marweentimbreza <marweenjosephtimbreza@gmail.com>
2025-06-14 22:03:10 +0800

    Overhauld the entire thing yet again. Made it much more simpler in file structure.

--- init.lua
+++ init.lua
@@ -1,1 +1,31 @@
-require("mj")
+require 'core.extra'
+require 'core.keymaps'
+require 'core.options'
+
+-- Set up the Lazy plugin manager
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Set up plugins
+require('lazy').setup {
+    require 'plugins.lsp',
+    require 'plugins.mason',
+    require 'plugins.telescope',
+    require 'plugins.treesitter',
+    require 'plugins.none-ls',
+    require 'plugins.gitsigns',
+    require 'plugins.autocomplete',
+    require 'plugins.neotree',
+    require 'plugins.whichkey',
+    require 'plugins.todo-comment',
+}
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
---
+++ lua/core/extra.lua
@@ -0,0 +1,32 @@
+vim.highlight.priorities.semantic_tokens = 95 -- Or any number lower than 100, treesitter's priority level
+
+-- Appearance of diagnostics
+vim.diagnostic.config {
+  virtual_text = {
+    prefix = '●',
+    -- Add a custom format function to show error codes
+    format = function(diagnostic)
+      local code = diagnostic.code and string.format('[%s]', diagnostic.code) or ''
+      return string.format('%s %s', code, diagnostic.message)
+    end,
+  },
+  underline = false,
+  update_in_insert = true,
+  float = {
+    source = 'always', -- Or "if_many"
+  },
+  -- Make diagnostic background transparent
+  on_ready = function()
+    vim.cmd 'highlight DiagnosticVirtualText guibg=NONE'
+  end,
+}
+
+-- Highlight on yank
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
